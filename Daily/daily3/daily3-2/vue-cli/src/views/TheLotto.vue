6d55526ed48d7461e3be1995cb2e3c862dbef795
최지수 <cjjss11@naver.com>
2023-05-25 17:07:55 +0900

    데일리 실 3-2. Router 동적 인자전달 활용, 로또 추첨 어플리케이션 제작

---
+++ Daily/daily3/daily3-2/vue-cli/src/views/TheLotto.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>로또</h1>
+        <button @click="getLuckyNumbers">Get Lucky Numbers</button>
+        <p>{{ selectedLuckyNumbers }}</p>
+    </div>
+</template>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.20/lodash.min.js"></script>
+
+<script>
+
+export default {
+    name: 'TheLotto',
+    data: function () {
+        return {
+            sampleNums: [],
+            selectedLuckyNumbers: [],
+        }
+    },
+    methods: {
+        getLuckyNumbers: function () {
+            const numbers = _.range(1, 46)
+            this.sampleNums = _.sampleSize(numbers, this.$route.params.lottoNum)
+            this.selectedLuckyNumbers = _.sortBy(this.sampleNums)
+        }
+    }
+}
+</script>
---
+++ Daily/daily3/daily3-2/vue-cli/src/views/TheLunch.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>점심메뉴</h1>
+        <button @click="pickLunch">Pick Lunch</button>
+        <p>{{ selectedLunch }}</p>
+        <br>
+        <h2>로또를 뽑아보자</h2>
+        <button @click="pickLottoNum">Pick Lotto</button>
+    </div>
+</template>
+
+<script>
+import _ from 'lodash'
+
+export default {
+    name: 'TheLunch',
+    data: function () {
+        return {
+            lunch: ['떡볶이','탕수육','피자','치킨'],
+            selectedLunch: null,
+        }
+    },
+    methods: {
+        pickLunch: function () {
+            this.selectedLunch = _.sample(this.lunch)
+        },
+        pickLottoNum: function () {
+            this.$router.push({ name: 'TheLotto', params: { lottoNum: 6 }})
+        }
+    }
+}
+</script>
